e8242b5aff85e76ae1e8e087bff7308122dc4930
Zhilong Li <zl242@cornell.edu>
2019-02-09 09:54:49 -0500

    Django framework set up

---
+++ quickmed/quickmed/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for quickmed project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickmed.settings')
+
+application = get_wsgi_application()
